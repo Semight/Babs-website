7a7e56863bbf49b83e01104b577c65804c8e8010
semight <davidoluwasemiloorei@gmail.com>
2022-12-22 02:26:22 +0100

    code modified

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-let icon = document.getElementById("icon");
+// let icon = document.getElementById("icon");
 
-icon.onclick = function() {
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "./images/sun.png";
-    }else {
-        icon.src = "./images/moon.png";
-    }
-}
+// icon.onclick = function() {
+//     document.body.classList.toggle("dark-theme");
+//     if(document.body.classList.contains("dark-theme")){
+//         icon.classList.add("fa-solid fa-sun");
+//     }else {
+//         icon.classList.add("fa-solid fa-moon");
+//     }
+// }
+
+const body = document.querySelector("body")
+const toggle = document.querySelector("#toggle");
+const moonIcon = document.querySelector(".fa-moon");
+const sunIcon = document.querySelector(".fa-sun");
+
+toggle.addEventListener("change", () => {
+    body.classList.toggle("dark-theme");
+    moonIcon.className = moonIcon.className == "fa-solid fa-moon" ? "fa-solid fa-moon" : "fa-solid fa-moon";
+    sunIcon.className = moonIcon.className == "fa-solid fa-sun" ? "fa-solid fa-sun" : "fa-solid fa-sun";
+})
